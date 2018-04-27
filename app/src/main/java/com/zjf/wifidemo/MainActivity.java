ad57a53f71068298bc00ae7d75db8114116a3360
jiefazhuang <937661220@qq.com>
2018-04-27 11:46:38 +0800

    增加两个按钮
    Signed-off-by: jiefazhuang <937661220@qq.com>

--- app/src/main/java/com/zjf/wifidemo/MainActivity.java
+++ app/src/main/java/com/zjf/wifidemo/MainActivity.java
@@ -2,8 +2,12 @@ package com.zjf.wifidemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnCreate = null;
+    private Button mBtnClose = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
